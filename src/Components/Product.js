5044c813de2371c66386918f36e28bdf44515403
Mani <maniramznpoor2007@gmail.com>
2022-08-21 08:48:42 +0430

    add module css to components

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,7 +1,7 @@
-import './Product.css'
+import styles from './Product.module.css'
 const Product = (props) => {
     return (
-        <div className="Product">
+        <div className={styles.Product}>
             <h1>Product Name : {props.name}</h1>
             <h2>Price is : {props.price}</h2>
         </div>
